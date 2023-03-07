9bd8167e17385f961bbf78fd01268b817dcda629
James Purvis <110248591+JamesPurvis@users.noreply.github.com>
2023-03-07 12:26:13 -0700

    index page added home controller added updated web security config

--- src/main/java/me/jamespurvis/loanservicewebapp/LoanServiceWebAppApplication.java
+++ src/main/java/me/jamespurvis/loanservicewebapp/LoanServiceWebAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoanServiceWebAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoanServiceWebAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoanServiceWebAppApplication.class, args);
+    }
 
 }
